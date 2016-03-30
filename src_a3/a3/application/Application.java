79f71f4917ebfb772647007a8c9ce8cee0fc4996
cdtinney <colintinney@cmail.carleton.ca>
2016-03-30 12:57:31 -0400

    add naive back in (there is an error in matrix calc)

--- src_a3/a3/application/Application.java
+++ src_a3/a3/application/Application.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import a3.classifiers.NaiveBayesClassifier;
 import a3.classifiers.OptimalBayesianClassifier;
 import a3.data.CSVParser;
 import a3.data.Partitioner;
@@ -13,12 +14,32 @@ import a3.model.Iris;
 public class Application {
 	
     public static void main(String[] args) throws InterruptedException, IOException {
+
+    	System.out.println("\nNaive Bayesian Classifier");
+    	testNaiveBayesianClassifier();
     	
+    	System.out.println("\nOptimal Bayesian Classifier");
     	testOptimalBayesianClassifier();
         
     }
     
-    private static void testOptimalBayesianClassifier() throws IOException {
+    private static void testNaiveBayesianClassifier() throws IOException {
+    	
+    	List<Iris> irisData = CSVParser.parseIrisDataset();
+    	
+    	// Split it into classes
+    	Map<String, List<Iris>> irisDataClassMap = Partitioner.getClassMap(irisData);
+    	
+    	// Partition into training/test sets
+    	PartitionedData partitionedData = Partitioner.getPartitionedData(10, irisDataClassMap);
+    	
+    	// Classify test data
+    	NaiveBayesClassifier nbClassifier = new NaiveBayesClassifier();
+    	nbClassifier.classify(partitionedData);
+		
+	}
+
+	private static void testOptimalBayesianClassifier() throws IOException {
     	
     	List<Iris> irisData = CSVParser.parseIrisDataset();
     	
--- src_a3/a3/classifiers/NaiveBayesClassifier.java
+++ src_a3/a3/classifiers/NaiveBayesClassifier.java
@@ -1,14 +1,68 @@
 package a3.classifiers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import a3.data.Partitioner.PartitionedData;
 import a3.model.Iris;
 
 public class NaiveBayesClassifier extends Classifier {
 	
-	public Map<String, ClassParameters> calcParameters(List<Iris> data) {
+	public Map<String, List<Iris>> classify(PartitionedData partitionedData) {
+		
+		// Calculate parameters
+    	Map<String, ClassParameters> classParamMap = calcParameters(partitionedData.getTrainingData());
+		
+		// Initialize the result map
+		Map<String, List<Iris>> result = new HashMap<String, List<Iris>>();
+		
+		// Classify test data
+		partitionedData.getTestData().forEach(iris -> {
+			
+			String bestClass = null;
+			double minDist = Double.MAX_VALUE;
+			
+			classParamMap.keySet().forEach(c -> {
+				
+				ClassParameters params = classParamMap.get(c);
+				
+				// Calculate the delta between dimensions and mean
+				double[] delta = calcDelta(iris.getFeatures(), params.getMeanVector());
+				
+				// Inverse the covariance matrix
+				RealMatrix m = MatrixUtils.createRealMatrix(params.getCovarianceMatrix());
+				RealMatrix mInv = new LUDecomposition(m).getSolver().getInverse();
+				double[][] inv = mInv.getData();
+				
+				// Transpose delta (turns into a 2D array)
+//				double[][] tDelta = transposeDelta(delta);
+//				
+//				// TODO - Calculate the Mahalanobis distance
+//				double[][] dotDeltaInv = dot(tDelta, inv);
+				
+				// TODO - If d < minDist, set minDist and bestClass
+				
+			});
+			
+			if (!result.containsKey(bestClass)) {
+				result.put(bestClass, new ArrayList<Iris>());
+			}
+			
+			result.get(bestClass).add(iris);
+			
+		});
+		
+		return result;
+		
+	}
+	
+	private Map<String, ClassParameters> calcParameters(List<Iris> data) {
 		
 		// Initialize the map to store mean vector/covariance for each class
 		Map<String, ClassParameters> statsMap = new HashMap<String, ClassParameters>();
